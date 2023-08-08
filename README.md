8cb302c1c76f16418f531575557459ade5fbe212
Ze Li <lize@guangnianai.com>
2023-08-08 16:51:01 +0800

    refactorize

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gpt-players
 Play with GPT and more.
+
+## chat
+
+```bash
+python -m gplay.chat --engine gpt-4
+```
--- gplay/chat.py
+++ gplay/chat.py
@@ -2,79 +2,9 @@ import os
 import json
 import openai
 
-
-def setup_pandafan_proxy():
-    os.environ['HTTP_PROXY'] = 'http://127.0.0.1:10080'
-    os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:10080'
-
-
-GPT_CALLABLE_FUNCTION_DESCRIPTIONS = []
-GPT_CALLABLE_FUNCTION_TABLE = dict()
-
-
-def gpt_callable(function: callable):
-    """
-    A decorator that registers a function as a callable for GPT to call.
-    """
-    global GPT_CALLABLE_FUNCTION_DESCRIPTIONS
-    global GPT_CALLABLE_FUNCTION_TABLE
-
-    # Get the parameters of the function
-    # https://stackoverflow.com/a/25959545/1165181
-    import inspect
-    signature = inspect.signature(function)
-    parameters = signature.parameters
-    required_parameters = []
-    for name, parameter in parameters.items():
-        if parameter.default is inspect.Parameter.empty:
-            required_parameters.append(name)
-
-    # Get the type of each parameter by parsing the signature
-    # https://stackoverflow.com/a/25959545/1165181
-    json_schema_types = dict()
-    type_annotation_to_json_schema_type = {
-        str: 'string',
-        int: 'integer',
-        float: 'number',
-        bool: 'boolean',
-    }
-    for name, parameter in parameters.items():
-        if parameter.annotation is not inspect.Parameter.empty:
-            type_annotation = parameter.annotation
-            json_schema_types[name] = type_annotation_to_json_schema_type.get(
-                type_annotation, 'string')
-
-    # Get the description of each parameter by parsing the doc
-    # https://stackoverflow.com/a/10079854/1165181
-    import re
-    doc = function.__doc__
-    if doc is None:
-        doc = ''
-    parameter_descriptions = {}
-    for name in parameters:
-        pattern = rf'{name}:(.*)'
-        match = re.search(pattern, doc)
-        if match:
-            parameter_descriptions[name] = match.group(1).strip()
-
-    # Register the function
-    GPT_CALLABLE_FUNCTION_DESCRIPTIONS.append({
-        'name': function.__name__,
-        'description': function.__doc__.split('\n\n')[0],
-        'parameters': {
-            'type': 'object',
-            'properties': {
-                name: {
-                    'type': json_schema_types.get(name, 'string'),
-                    'description': parameter_descriptions.get(name, ''),
-                } for name in parameters
-            },
-            'required': required_parameters,
-        },
-    })
-    GPT_CALLABLE_FUNCTION_TABLE[function.__name__] = function
-
-    return function
+from .util import (
+    gpt_callable, get_gpt_callable_function_descriptions, get_gpt_callable_function,
+    setup_pandafan_proxy)
 
 
 @gpt_callable
@@ -194,7 +124,8 @@ def stream_chat_completion(engine: str, messages: list, temperature: float,
 def run_chat(engine: str, t: float = 1.0):
     messages = [
         {"role": "system",
-         "content": "You are a helpful assistant. When calling functions, only use the functions you have been provided with."},
+         "content": ("You are a helpful assistant. When calling functions, "
+                     "only use the functions you have been provided with.")},
     ]
     while True:
         user_content = input("[user]: ")
@@ -207,7 +138,7 @@ def run_chat(engine: str, t: float = 1.0):
                 engine=engine,
                 messages=messages,
                 temperature=t,
-                functions=GPT_CALLABLE_FUNCTION_DESCRIPTIONS,
+                functions=get_gpt_callable_function_descriptions(),
                 function_call="auto",  # auto is default, but we'll be explicit
             )
 
@@ -218,7 +149,8 @@ def run_chat(engine: str, t: float = 1.0):
                 function_name = new_message["function_call"]["name"]
                 print(f'[system]: Calling function {function_name} ...')
 
-                function_to_call: callable = GPT_CALLABLE_FUNCTION_TABLE[function_name]
+                function_to_call: callable = get_gpt_callable_function(
+                    function_name)
                 function_args = new_message["function_call"]["arguments"]
                 if function_args is None:
                     function_args = dict()
